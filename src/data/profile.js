c19ce459d9a20d45d0894824ffbf1b52a7460d21
Kavathiya-Harsh <harsh.kavathiya.cg@gmail.com>
2026-04-17 22:24:17 +0530

    resume update

--- src/data/profile.js
+++ src/data/profile.js
@@ -2,5 +2,5 @@ export const profile = {
   name: 'Harsh Kavathiya',
   photoUrl: 'https://res.cloudinary.com/dvv5mtpli/image/upload/q_auto,f_auto,w_600,c_fill,g_face/v1776351778/Harsh_Kavathiya_Profile_duaqrs.jpg', // Balanced for Mobile/Desktop LCP
   initials: 'HK',
-  resumeUrl: 'https://res.cloudinary.com/dvv5mtpli/image/upload/q_auto/f_auto/v1774873108/Minimalist_White_and_Grey_Professional_Resume_2__page-0001_1_wbwbzu.jpg',
+  resumeUrl: 'https://drive.google.com/file/d/1uhRrxaeVKePvxesP23Cr2YB0n0n-fD-O/view?usp=drive_link',
 };
