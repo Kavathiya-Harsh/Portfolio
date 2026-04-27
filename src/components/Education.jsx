660f484179bc09d2f3a9b2825692f8004ee533ff
Kavathiya-Harsh <harsh.kavathiya.cg@gmail.com>
2026-04-27 12:06:23 +0530

    update

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,8 +5,21 @@ import { education } from '../data/education';
 import { fadeInUp, staggerContainer, viewportOnce } from '../utils/motion';
 
 function EducationCard({ item, index }) {
+  const cardRef = React.useRef(null);
+
+  const handleMouseMove = (e) => {
+    if (!cardRef.current) return;
+    const rect = cardRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    cardRef.current.style.setProperty('--mouse-x', `${x}px`);
+    cardRef.current.style.setProperty('--mouse-y', `${y}px`);
+  };
+
   return (
     <motion.div
+      ref={cardRef}
+      onMouseMove={handleMouseMove}
       variants={fadeInUp}
       className="relative group lg:pl-8 pb-12 last:pb-0"
     >
