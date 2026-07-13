ea81db2da770ad01b37fa9b722daedd28a5a13e3
Harsh_Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-07-13 10:11:38 +0530

    profile photo update

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,6 +1,6 @@
 export const profile = {
   name: 'Harsh Kavathiya',
-  photoUrl: '/photo.jpg',
+  photoUrl: '/harsh.png',
   initials: 'HK',
   resumeUrl: 'https://drive.google.com/file/d/1uhRrxaeVKePvxesP23Cr2YB0n0n-fD-O/view?usp=drive_link',
 };
